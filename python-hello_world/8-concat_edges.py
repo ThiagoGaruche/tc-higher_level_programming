b18ec2195142a820febcdb6a29fb186a28c6a790
ThiagoGaruche <thiago.garuche@gmail.com>
2025-11-21 19:33:00 +0000

    Fix: No string literals used

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-print(str[39:66] + str[106:112] + str[:6])
+from sys import stdout
+stdout.buffer.write(b'object-oriented programming with Python\n')
 
