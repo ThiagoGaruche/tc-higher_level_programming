a65dcc79a37ca3e9cd68f8a50cadb37516b9efa2
ThiagoGaruche <thiago.garuche@gmail.com>
2025-11-21 18:59:53 +0000

    Complete 8-concat_edges.py with correct original format

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-text = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-print(text[39:66] + text[106:112] + text[:6])
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+print(str[39:66] + str[106:112] + str[:6])
 
