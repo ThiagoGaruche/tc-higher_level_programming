62414419017d49b49068498fab023f3844414c7f
ThiagoGaruche <thiago.garuche@gmail.com>
2025-11-21 19:35:10 +0000

    Add execute permission to 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-from sys import stdout
-stdout.buffer.write(b'object-oriented programming with Python\n')
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+print(str[39:66] + str[106:112] + str[:6])
 
