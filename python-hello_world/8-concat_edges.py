aa0754a30174936716346816244aaecc7b57325a
ThiagoGaruche <thiago.garuche@gmail.com>
2025-11-21 19:16:20 +0000

    Corrige 8-concat_edges.py sem criar variáveis novas

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-print("Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"[39:66] +
-      "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"[106:112] +
-      "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"[:6])
+print("Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"[39:66] + "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"[106:112] + "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"[:6])
 # End of file
