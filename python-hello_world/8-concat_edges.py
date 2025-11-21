d3b9cb2f33b0443aae8d6685dafe33651c89177d
ThiagoGaruche <thiago.garuche@gmail.com>
2025-11-21 18:56:43 +0000

    Fix variable name in 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-print(str[39:66] + str[106:112] + str[:6])
-
+text = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+print(text[39:66] + text[106:112] + text[:6])
 
